c9d4042f8221bd57104e60b2da6f959ecfd3c7e8
jeisson <jeisson@Jeisson>
2017-03-12 18:26:59 -0500

    ZC

--- src/practicando/Club.java
+++ src/practicando/Club.java
@@ -9,21 +9,35 @@ package practicando;
  *
  * @author Estudiante
  */
+/**
+esta es la clase club
+*/
 public class Club {
     private String nombre;
     public Persona gerente;
-
+/**
+este metodo es el constructor
+*/
     public Club(String nombre) {
         this.nombre = nombre;
     }
-
+/**
+este metodo es el get,para obtener informacion
+*/
     public String getNombre() {
         return nombre;
     }
-
+/**
+este metodo es el set,para cambiar informacion
+* @param nombre indica el nombre que va a tener el objeto
+*/
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+    /**
+*este metodo es para imprimir informacion informacion
+*/
 void imprimir_gerente(){
 System.out.println("la gerente es ="+this.gerente.getNombre());
 System.out.println("la gerente tiene " +this.gerente.getEdad()+" años");
